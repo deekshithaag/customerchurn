ddb87a5743cd225127040c8106c67b9c124ac9f0
Deekshitha <ag.deekkshitha@gmail.com>
2023-08-28 21:45:39 +0530

    hyperparamter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,15 +45,56 @@ class ModelTrainer:
                 "Random Forest": RandomForestClassifier(),
                 "Decision Tree": DecisionTreeClassifier(),
                 "Logistic Regression": LogisticRegression(),
-                "Extra Trees Classifier": ExtraTreesClassifier(),
+                #"Extra Trees Classifier": ExtraTreesClassifier(),
                 "XGB Classifier": XGBClassifier(),
                 "Bagging Classifier": BaggingClassifier(),
                 "AdaBoost Classifier": AdaBoostClassifier(),
                 "GradientBoosting Classifier": GradientBoostingClassifier()
             }
+            params={
+                
+                "Random Forest":{
+                    'n_estimators':[10,20,30,50],'criterion':['gini','entropy'],'max_depth':[2,3,5,6],
+                  'min_samples_split':[2,3,4,5],'min_samples_leaf':[1,3,5],'max_leaf_nodes':[2,3,4]
+                },
+                "Decision Tree": {
+                    'criterion':['gini','entropy'],
+                  'max_depth':[2,3,5,6],
+                  'min_samples_split':[2,3,4,5],
+                  'min_samples_leaf':[1,3,5],
+                  'max_leaf_nodes':[2,3,4],
+                  'max_features':['auto','sqrt','log2']
+                },
+                "Logistic Regression":{
+                    'penalty':['l1','l2'],'max_iter':[30,50,100,120,150]
+                    },
+                "XGB Classifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Bagging Classifier":{
+                    'base_estimator':[RandomForestClassifier(), LogisticRegression()],
+                    'n_estimators':[10, 20, 40],
+                    'max_samples':[1,2,3],
+                    'max_features':[1,2,3]
+                },
+                 "AdaBoost Classifier":{
+                    'base_estimator':[RandomForestClassifier(),DecisionTreeClassifier()],'n_estimators':[30,50,80,100],
+                 'learning_rate':[0.5,1,1.5,2]
+                }
+                ,
+                "GradientBoosting Classifier":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
 
+            }
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
-                                             models=models)
+                                             models=models,param=params)
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
 
